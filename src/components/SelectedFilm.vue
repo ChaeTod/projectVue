88415dface0f55a2a59e6f9cba867905507ed8db
Artem Kozyr <zymber6@yahoo.com>
2020-05-24 18:22:11 +0300

    Made example of GET data from API in json.

---
+++ src/components/SelectedFilm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="selectedFilm">
+    <div class="style">Title: {{film.title}}</div>
+    <div class="style">Episode # {{film.episode_id}}</div>
+    <div class="style">Opening crawl: {{film.opening_crawl}}</div>
+    <div class="style">Director: {{film.director}}</div>
+    <div class="style">Producer: {{film.producer}}</div>
+    <div class="style">Release date: {{film.release_date}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      film: {}
+    };
+  },
+  created() {
+    fetch("https://swapi.dev/api/films/" + this.id)
+      .then(response => response.json())
+      .then(data => {
+        this.film = data;
+      });
+  }
+};
+</script>
+
+<style>
+#selectedFilm {
+  columns: 1;
+  column-gap: 30px;
+  column-rule: 1px solid black;
+  column-span: all;  
+  
+  width: 100%;
+  border: 2px solid #ccc;
+  padding: 10px;
+  margin-bottom: 30px;
+  max-width: 35%;
+  margin: 10% auto;
+}
+.style {
+  text-align: center;
+  width: 100%;
+  font-size: 1.4vw;
+  padding: 10px;
+  margin: 0px 0;
+  border: 1px solid black;
+  box-sizing: border-box;
+  background: lightblue;
+}
+</style>
